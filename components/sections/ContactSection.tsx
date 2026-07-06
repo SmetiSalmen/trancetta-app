e03580600f7eb83070a02c8d27013e695a7bb227
SmetiSalmen <salmen.smeti.12@gmail.com>
2026-07-06 12:59:34 +0100

    update Trancetta website

--- components/sections/ContactSection.tsx
+++ components/sections/ContactSection.tsx
@@ -151,7 +151,8 @@ export default function ContactSection() {
           className="
             grid
             grid-cols-1
-            md:grid-cols-3
+            md:grid-cols-2
+            lg:grid-cols-4
             gap-5
             mb-10
           "
